84840b578811b054c90bd3ce27cc9e8e05121e70
aleoli05 <aleoli05@gmail.com>
2026-04-14 20:05:34 -0300

    Commit 20260414 RCum

--- R/Plot_Ratio_Horizon.R
+++ R/Plot_Ratio_Horizon.R
@@ -2667,9 +2667,9 @@ Plot_Annualized_RCum_Horizon <-function(){
     Analyzis[i,j+1]=Vitories
     Analyzis[i,j+2]=round(mean(Analyzis[i,1:ncol(Base_Dif)]),2)
     if (Return_Cumulative=='Rebalanced'){
-      Analyzis[i,j+3]=round((prod(Analyzis[i,1:ncol(Base_Dif)]/100+1)-1),2)
+      Analyzis[i,j+3]=round((prod(Base_Dif[i,1:ncol(Base_Dif)]/100+1)-1),2)
     }else{
-      Analyzis[i,j+3]=round((prod(Analyzis[i,1:ncol(Base_Dif)]/100+1)-1)/nrow(Base_Dif),2)
+      Analyzis[i,j+3]=round((prod(Base_Dif[i,1:ncol(Base_Dif)]/100+1)-1)/nrow(Base_Dif),2)
     }
   }
   for(j in 1:(ncol(Base_Dif)+3)){
