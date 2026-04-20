1e075e8fe102491ce36e989d7bc71fe92c51895a
aleoli05 <aleoli05@gmail.com>
2026-04-20 10:42:10 -0300

    Commit 20260420 cov.mat

--- R/Gen_portfolios.R
+++ R/Gen_portfolios.R
@@ -485,7 +485,7 @@ if ((ncol(TodosAtivosPredict)<nrow(TodosAtivosPredict))==TRUE){
     weight_GMV = pesos_todosPredict
   }else{
     EPR=colMeans(TodosAtivosPredict)
-    COV=nearPD(cov(TodosAtivosPredict)$mat)
+    COV=as.matrix(nearPD(cov(TodosAtivosPredict)$mat))
     GMV=globalMin.portfolio(EPR,COV)
     GMV_Return = GMV$er
     GMV_sd = GMV$sd
--- R/Out_of_sample.R
+++ R/Out_of_sample.R
@@ -149,8 +149,8 @@ Out_of_sample <-function(Initial_Date_Out, Final_Date_Out){
   # GMV - Global Minimum Variance
   if(nrow(TodosAtivosPredict)>ncol(TodosAtivosPredict)){
   EPR=colMeans(TodosAtivosPredict)
-  COV=nearPD(cov(TodosAtivosPredict)$mat)
-  COV=var(TodosAtivosPredict)
+  COV=as.amatrix(nearPD(cov(TodosAtivosPredict)$mat))
+  #COV=var(TodosAtivosPredict)
   GMV=globalMin.portfolio(EPR,COV)
   GMV_Return = GMV$er
   GMV_sd = GMV$sd
